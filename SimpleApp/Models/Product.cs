31d2fbc131ca9bc5aa5038b4efe9a144f4e8a15d
TihonchukArtyom89 <mortus1989@gmail.com>
2023-07-15 15:44:42 +0300

    work on isolating comзщnent(сщтекщддук),add interface to models in simpleapp project,add class productdatasource(based on interface idatasource),comment method products in class product,rewrite controller,add test data source(fakedatasource class in homecontrollertests)

--- SimpleApp/Models/Product.cs
+++ SimpleApp/Models/Product.cs
@@ -2,20 +2,37 @@
 
 public class Product
 {
-    public string Name { get; set; }=string.Empty;
+    public string Name { get; set; } = string.Empty;
     public decimal? Price { get; set; }
-    public static Product[] GetProducts()
-    {
-        Product kayak = new Product
+    //public static Product[] GetProducts()
+    //{
+    //    Product kayak = new Product
+    //    {
+    //        Name = "Kayak",
+    //        Price = 275M
+    //    };
+    //    Product lifejacket = new Product
+    //    {
+    //        Name = "Lifejacket",
+    //        Price = 48.95M
+    //    };
+    //    return new Product[] { kayak, lifejacket };
+    //}    
+}
+public class ProductDataSource : IDataSource
+{
+    public IEnumerable<Product> Products =>
+        new Product[]
         {
-            Name = "Kayak",
-            Price = 275M
+            new Product
+            {
+                Name = "Kayak",
+                Price = 275M
+            },
+            new Product
+            {
+                Name = "Lifejacket",
+                Price = 48.95M
+            }
         };
-        Product lifejacket = new Product
-        {
-            Name = "Lifejacket",
-            Price = 48.95M
-        };
-        return new Product[] { kayak, lifejacket };
-    }
 }
