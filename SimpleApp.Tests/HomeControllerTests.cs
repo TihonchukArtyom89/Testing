2d7b0b3e739aa49cc8c60cfcff03572f270294f4
TihonchukArtyom89 <mortus1989@gmail.com>
2023-07-15 16:05:05 +0300

    work on using moq,add moq package,rewrite in homecontrollertest indexactionmodeliscomplete with using moq

--- SimpleApp.Tests/HomeControllerTests.cs
+++ SimpleApp.Tests/HomeControllerTests.cs
@@ -7,16 +7,17 @@ using Microsoft.AspNetCore.Mvc;
 using SimpleApp.Controllers;
 using SimpleApp.Models;
 using Xunit;
+using Moq;
 
 namespace SimpleApp.Tests;
 
 public class HomeControllerTests
 {
-    class FakeDataSource : IDataSource
-    {
-        public FakeDataSource(Product[] data) => Products = data;
-        public IEnumerable<Product> Products { get; set; }
-    }
+    //class FakeDataSource : IDataSource
+    //{
+    //    public FakeDataSource(Product[] data) => Products = data;
+    //    public IEnumerable<Product> Products { get; set; }
+    //}
     [Fact]
     //public void IndexActionModelIsComplete()//prev code
     //{
@@ -41,13 +42,16 @@ public class HomeControllerTests
             new Product{ Name="P2",Price=120M},
             new Product{ Name="P3",Price=110M}
         };
-        IDataSource data = new FakeDataSource(testData);
+        var mock = new Mock<IDataSource>();
+        mock.SetupGet(m => m.Products).Returns(testData);
+        //IDataSource data = new FakeDataSource(testData);
         var controller = new HomeController();
-        controller.dataSource = data;
+        controller.dataSource = mock.Object;
         //Act
         var model = (controller.Index() as ViewResult)?.ViewData.Model as IEnumerable<Product>;
         //Assert
-        Assert.Equal(data.Products, model, Comparer.Get<Product>((p1, p2) => p1?.Name == p2?.Name && p1?.Price == p2?.Price));
+        Assert.Equal(testData, model, Comparer.Get<Product>((p1, p2) => p1?.Name == p2?.Name && p1?.Price == p2?.Price));
+        mock.VerifyGet(m=>m.Products,Times.Once());
     }
 }
 
